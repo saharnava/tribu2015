d20bdc24110ad2060db30d649ca13e256adfea85
anneclaire <ac.morcrette@gmail.com>
2017-06-09 10:58:12 +0200

    Nouvelle App ID

--- amp/single.php
+++ amp/single.php
@@ -42,7 +42,7 @@
 			<div class="robots-nocontent sd-block sd-social sd-social-icon sd-sharing">
 				<h3 class="sd-title">Partager sur</h3>
 				<div class="sd-content">
-					<amp-social-share type="facebook" data-param-app_id="1860449257551677" width="40" height="40" class="sd-button"></amp-social-share>
+					<amp-social-share type="facebook" data-param-app_id="1874168346189094" width="40" height="40" class="sd-button"></amp-social-share>
 					<amp-social-share type="pinterest" data-param-media="<?php echo get_the_post_thumbnail_url(); ?>" width="40" height="40" class="sd-button"></amp-social-share>
 					<amp-social-share type="twitter" width="40" height="40" class="sd-button"></amp-social-share>
 					<amp-social-share type="whatsapp" width="40" height="40" class="sd-button"></amp-social-share>
